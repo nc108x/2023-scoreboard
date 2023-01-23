5b8c6710fbe8f8e14ce37e955aba304a2a7cc998
Nick Choi <91555532+nc108x@users.noreply.github.com>
2023-01-24 01:11:09 +0800

    working score calculation

--- gamefield.js
+++ gamefield.js
@@ -1,51 +1,64 @@
-import { redDragon, blueDragon } from './score.js'
-
-class Poles {
-    constructor(type, color) {
+import { update_score } from './score.js'
+/* rings: array containing all the rings thrown onto the pole */
+/* am using it as a stack so the last element is the topmost ring i.e. the ring that is actually counted */
+/* type: contains the type of the pole (type 1/2/3 and which side of the field it is on for type 1) */
+class Pole {
+    rings = []
+    constructor(type) {
         this.type = type
-        this.color = color
-        this.rings = []
     }
 }
+
+/* array containing all the pole objects */
+/* score.js will access this to check the state of the poles and calc the score */
+const poles = []
 
 /* triggered when poles are clicked */
 /* left click: red  */
 /* right click: blue */
-function score_listener(event, color, dragon) {
+function score_listener(event, color, pole_no) {
     /* prevents the right click menu from showing up */
     event.preventDefault()
 
-    console.log(color)
+    /* console.log(color) */
 
-    if (event.target.className.includes('type1')) {
-        console.log('type1')
-        dragon.score++
-    } else if (event.target.className.includes('type2')) {
-        console.log('type2')
-        dragon.score += 2
-    } else if (event.target.className.includes('type3')) {
-        console.log('type3')
-        dragon.score += 3
-    }
+    /* update specified pole */
+    poles[pole_no].rings.push(color)
+    console.log(pole_no)
+    console.log(poles[pole_no].rings)
+    update_score()
 
-    if (color == 'red') {
-        document.getElementById('red_score').innerHTML = dragon.score
-    } else {
-        document.getElementById('blue_score').innerHTML = dragon.score
-    }
+    /* if (event.target.className.includes('type1')) { */
+    /*     console.log('type1') */
+    /* } else if (event.target.className.includes('type2')) { */
+    /*     console.log('type2') */
+    /* } else if (event.target.className.includes('type3')) { */
+    /*     console.log('type3') */
+    /* } */
+
+    /* if (color == 'red') { */
+    /*     document.getElementById('red_score').innerHTML = dragon.score */
+    /* } else { */
+    /*     document.getElementById('blue_score').innerHTML = dragon.score */
+    /* } */
 }
 
-/* basically inits all gamefield objects */
-export function gamefield_init() {
-    /* add pole listeners */
+/* basically inits all gamefield stuff */
+function gamefield_init() {
     const pole_buttons = document.getElementsByClassName('pole')
 
     for (let i = 0; i < pole_buttons.length; i++) {
+        /* create pole objects */
+        poles[i] = new Pole(pole_buttons[i].className.substring(4))
+
+        /* add pole button listeners for left/right click */
         pole_buttons[i].addEventListener('click', (event) => {
-            score_listener(event, 'red', redDragon)
+            score_listener(event, 'red', i)
         })
         pole_buttons[i].addEventListener('contextmenu', (event) => {
-            score_listener(event, 'blue', blueDragon)
+            score_listener(event, 'blue', i)
         })
     }
 }
+
+export { poles, gamefield_init }
--- score.js
+++ score.js
@@ -1,11 +1,74 @@
+import { poles } from './gamefield.js'
+/* name: fiery/war dragon (unless we come up with new names lmao) */
+/* score: current score */
+/* there should only be two instances of the Dragon object (red and blue) */
 class Dragon {
-    constructor(name, score, violations) {
+    constructor(name, score) {
         this.name = name
         this.score = score
-        this.violations = violations
     }
 }
 
 /* define team objects */
-export let redDragon = new Dragon('Fiery', 0, 0)
-export let blueDragon = new Dragon('War', 0, 0)
+let redDragon = new Dragon('Fiery', 0)
+let blueDragon = new Dragon('War', 0)
+
+/* updates score saved in the team objects and the display on the scoreboard */
+/* does so by checking each pole to see who has the topmost ring */
+/* I am aware that this is not the most efficient way to do it but eh */
+function update_score() {
+    redDragon.score = 0
+    blueDragon.score = 0
+
+    for (let i = 0; i < poles.length; i++) {
+        /* skip if the pole is empty */
+        if (poles[i].rings.length == 0) {
+            /* console.log('empty') */
+            continue
+        }
+
+        /* console.log( */
+        /*     'pole number ' + */
+        /*         i + */
+        /*         ': ' + */
+        /*         poles[i].rings[poles[i].rings.length - 1] */
+        /* ) */
+
+        let captured_by = poles[i].rings[poles[i].rings.length - 1]
+        let score_increase = 0
+
+        if (poles[i].type.includes('type1')) {
+            /* console.log('type1') */
+
+            /* consider type 1 bonus for opposing pole */
+            if (poles[i].type.includes(captured_by)) {
+                score_increase = 10
+            } else {
+                score_increase = 25
+            }
+        } else if (poles[i].type.includes('type2')) {
+            /* console.log('type2') */
+
+            score_increase = 30
+        } else if (poles[i].type.includes('type3')) {
+            /* console.log('type3') */
+
+            score_increase = 70
+        }
+
+        console.log('score increase: ' + score_increase)
+
+        /* add score to team  */
+        if (captured_by == 'red') {
+            redDragon.score += score_increase
+        } else {
+            blueDragon.score += score_increase
+        }
+
+        /* update score displayed on the scoreboard */
+        document.getElementById('red_score').innerHTML = redDragon.score
+        document.getElementById('blue_score').innerHTML = blueDragon.score
+    }
+}
+
+export { update_score }
