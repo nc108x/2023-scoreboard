21aeaa913e50ed49da5b2009a0c6ae778875a9c4
Nick Choi <91555532+nc108x@users.noreply.github.com>
2023-02-09 00:49:02 +0800

    pole buttons now change color

--- control_panel.js
+++ control_panel.js
@@ -1,36 +1,42 @@
-import { redDragon, blueDragon, poles, update_score } from "./score.js"
+import { redDragon, blueDragon, poles, update_score } from "./score.js";
 
 /* resets the state of all the poles */
 function reset_listener() {
-    console.log("reset")
-    for (let i = 0; i < poles.length; i++) {
-        poles[i].rings.length = 0
-        console.log(poles[i].rings)
-    }
-    update_score()
+  console.log("reset");
+  let pole_buttons = document.getElementsByClassName("pole");
+
+  for (let i = 0; i < pole_buttons.length; i++) {
+    pole_buttons[i].style.background = "#FFFFFF";
+  }
+
+  for (let i = 0; i < poles.length; i++) {
+    poles[i].rings.length = 0;
+    console.log(poles[i].rings);
+  }
+  update_score();
 }
 
 /* swap names of the two sides */
 function swap_listener() {
-    if (redDragon.name == "FIERY") {
-        redDragon.name = "WAR"
-        blueDragon.name = "FIERY"
-    } else {
-        redDragon.name = "FIERY"
-        blueDragon.name = "WAR"
-    }
-    document.getElementById("red-name").innerHTML = redDragon.name + " DRAGON: "
-    document.getElementById("blue-name").innerHTML =
-        blueDragon.name + " DRAGON: "
+  if (redDragon.name == "FIERY") {
+    redDragon.name = "WAR";
+    blueDragon.name = "FIERY";
+  } else {
+    redDragon.name = "FIERY";
+    blueDragon.name = "WAR";
+  }
+  document.getElementById("red-name").innerHTML = redDragon.name + " DRAGON: ";
+  document.getElementById("blue-name").innerHTML =
+    blueDragon.name + " DRAGON: ";
 }
 
 /* inits all buttons in the control panel */
 function control_panel_init() {
-    let reset_button = document.getElementById("reset")
-    reset_button.addEventListener("click", reset_listener)
+  let reset_button = document.getElementById("reset");
+  reset_button.addEventListener("click", reset_listener);
 
-    let swap_button = document.getElementById("swap")
-    swap_button.addEventListener("click", swap_listener)
+  let swap_button = document.getElementById("swap");
+  swap_button.addEventListener("click", swap_listener);
 }
 
-export { control_panel_init }
+export { control_panel_init };
--- score.js
+++ score.js
@@ -3,113 +3,114 @@
 /* there should only be two instances of the Dragon object (red and blue) */
 class Dragon {
     constructor(name, score) {
-        this.name = name
-        this.score = score
+        this.name = name;
+        this.score = score;
     }
 }
 
 /* define team objects */
-let redDragon = new Dragon("FIERY", 0)
-let blueDragon = new Dragon("WAR", 0)
+let redDragon = new Dragon("FIERY", 0);
+let blueDragon = new Dragon("WAR", 0);
 
 /* rings: array containing all the rings thrown onto the pole */
 /* am using it as a stack so the last element is the topmost ring i.e. the ring that is actually counted */
 /* type: contains the type of the pole (type 1/2/3 and which side of the field it is on for type 1) */
 class Pole {
-    rings = []
+    rings = [];
     constructor(type) {
-        this.type = type
+        this.type = type;
     }
 }
 
 /* array containing all the pole objects */
-const poles = []
+const poles = [];
 
 /* updates score saved in the team objects and the display on the scoreboard */
 /* does so by checking each pole to see who has the topmost ring */
 /* I am aware that this is not the most efficient way to do it but eh */
 function update_score() {
-    redDragon.score = 0
-    blueDragon.score = 0
+    redDragon.score = 0;
+    blueDragon.score = 0;
 
     for (let i = 0; i < poles.length; i++) {
-        let score_increase = 0
+        let score_increase = 0;
 
         /* skip if the pole is empty */
         if (poles[i].rings.length != 0) {
-            let captured_by = poles[i].rings[poles[i].rings.length - 1]
+            let captured_by = poles[i].rings[poles[i].rings.length - 1];
 
             if (poles[i].type.includes("type1")) {
                 /* console.log('type1') */
 
                 /* consider type 1 bonus for opposing pole */
                 if (poles[i].type.includes(captured_by)) {
-                    score_increase = 10
+                    score_increase = 10;
                 } else {
-                    score_increase = 25
+                    score_increase = 25;
                 }
             } else if (poles[i].type.includes("type2")) {
                 /* console.log('type2') */
 
-                score_increase = 30
+                score_increase = 30;
             } else if (poles[i].type.includes("type3")) {
                 /* console.log('type3') */
 
-                score_increase = 70
+                score_increase = 70;
             }
 
             console.log(
                 "pole " +
-                    i +
-                    " captured by " +
-                    captured_by +
-                    " with a score increase of " +
-                    score_increase
-            )
+                i +
+                " captured by " +
+                captured_by +
+                " with a score increase of " +
+                score_increase
+            );
 
             /* add score to team  */
             if (captured_by == "red") {
-                redDragon.score += score_increase
+                redDragon.score += score_increase;
             } else {
-                blueDragon.score += score_increase
+                blueDragon.score += score_increase;
             }
         }
     }
 
     /* update score displayed on the scoreboard */
-    document.getElementById("red-score").innerHTML = redDragon.score
-    document.getElementById("blue-score").innerHTML = blueDragon.score
+    document.getElementById("red-score").innerHTML = redDragon.score;
+    document.getElementById("blue-score").innerHTML = blueDragon.score;
 }
 
-function pole_button_listener(event, color, pole_no) {
+function pole_button_listener(event, color, pole_no, pole_html) {
     /* prevents the right click menu from showing up */
-    event.preventDefault()
+    event.preventDefault();
 
     /* console.log(color) */
 
     /* update specified pole */
-    poles[pole_no].rings.push(color)
-    console.log(pole_no)
-    console.log(poles[pole_no].rings)
-    update_score()
+    poles[pole_no].rings.push(color);
+    pole_html.style.background = color == "red" ? "#f5c2e7" : "#89b4fa";
+    console.log(pole_no);
+    console.log(poles[pole_no].rings);
+    update_score();
 }
 
 /* basically inits all gamefield stuff */
 function gamefield_init() {
-    const pole_buttons = document.getElementsByClassName("pole")
+    const pole_buttons = document.getElementsByClassName("pole");
 
     for (let i = 0; i < pole_buttons.length; i++) {
         /* create pole objects */
-        poles[i] = new Pole(pole_buttons[i].className.substring(4))
+        poles[i] = new Pole(pole_buttons[i].className.substring(4));
 
         /* add pole button listeners for left/right click */
         pole_buttons[i].addEventListener("click", (event) => {
-            pole_button_listener(event, "red", i)
-        })
+            pole_button_listener(event, "red", i, pole_buttons[i]);
+        });
         pole_buttons[i].addEventListener("contextmenu", (event) => {
-            pole_button_listener(event, "blue", i)
-        })
+            pole_button_listener(event, "blue", i, pole_buttons[i]);
+        });
     }
 }
 
-export { redDragon, blueDragon, poles, update_score, gamefield_init }
+export { redDragon, blueDragon, poles, update_score, gamefield_init };
